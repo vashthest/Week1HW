ab8a309ee74e9058f0a8e3f286d1ed1b6ff40358
Yi Wei <vashthest@gmail.com>
2014-08-04 20:12:04 -0700

    Initial Commit

---
+++ Week1HWTests/Week1HWTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week1HWTests.swift
+//  Week1HWTests
+//
+//  Created by Yi on 8/4/14.
+//  Copyright (c) 2014 Dropbox. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Week1HWTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
